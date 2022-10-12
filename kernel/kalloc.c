ad647feed01d4c75293393b602b7a1fed8eb4afa
parthdesai2207 <parthdesai2207@gmail.com>
2022-10-12 12:43:06 +0530

    Revert "Completed COW"
    This reverts commit 4c7f1a5ac09110503bcf135f29e6b443c44f69d0.

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -23,53 +23,9 @@ struct {
   struct run *freelist;
 } kmem;
 
-struct {
-  struct spinlock lock;
-  int count[PGROUNDUP(PHYSTOP)>>12];
-} page_ref;
-
-void init_page_ref(){
-  initlock(&page_ref.lock, "page_ref");
-  acquire(&page_ref.lock);
-  for(int i=0;i<(PGROUNDUP(PHYSTOP)>>12);++i)
-    page_ref.count[i]=0;
-  release(&page_ref.lock);
-}
-
-
-void dec_page_ref(void*pa){
-  acquire(&page_ref.lock);
-  if(page_ref.count[(uint64)pa>>12]<=0){
-    panic("dec_page_ref");
-  }
-  page_ref.count[(uint64)pa>>12]-=1;
-  release(&page_ref.lock);
-}
-
-void inc_page_ref(void*pa){
-  acquire(&page_ref.lock);
-  if(page_ref.count[(uint64)pa>>12]<0){
-    panic("inc_page_ref");
-  }
-  page_ref.count[(uint64)pa>>12]+=1;
-  release(&page_ref.lock);
-}
-
-int get_page_ref(void*pa){
-  acquire(&page_ref.lock);
-  int res = page_ref.count[(uint64)pa>>12];
-  if(page_ref.count[(uint64)pa>>12]<0){
-    panic("get_page_ref");
-  }
-  release(&page_ref.lock);
-  return res;
-}
-
-
 void
 kinit()
 {
-  init_page_ref();
   initlock(&kmem.lock, "kmem");
   freerange(end, (void*)PHYSTOP);
 }
@@ -79,13 +35,11 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE){
-    inc_page_ref(p);
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
     kfree(p);
-  }
 }
 
-// Free the page of physical memory pointed at by v,
+// Free the page of physical memory pointed at by pa,
 // which normally should have been returned by a
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
@@ -93,18 +47,10 @@ void
 kfree(void *pa)
 {
   struct run *r;
+
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
-  acquire(&page_ref.lock);
-  if(page_ref.count[(uint64)pa>>12]<=0){
-    panic("dec_page_ref");
-  }
-  page_ref.count[(uint64)pa>>12]-=1;
-  if(page_ref.count[(uint64)pa>>12]>0){
-    release(&page_ref.lock);
-    return;
-  }
-  release(&page_ref.lock);
+
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
@@ -130,9 +76,7 @@ kalloc(void)
     kmem.freelist = r->next;
   release(&kmem.lock);
 
-  if(r){
+  if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
-    inc_page_ref((void*)r);
-  }
   return (void*)r;
 }
